036a81a702e88cdcc61a2ebbbd635c089bd7f95a
Scotchrock <54104221+Scotchrock@users.noreply.github.com>
2019-08-16 10:08:47 +0300

    123

---
+++ start.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+
+</head>
+<body>
+<?php 
+			echo "Hello Scotch!";
+			?>
+			<?php
+			echo "Hello"."</br>" ;
+			?>
+			это первый мой код!
+			<?php
+					$str_1="первая строка";
+					$str_2="вторая строка";
+					echo $str_1. "".$str_2." \" \" \" ";
+					
+
+
+
+					?>
+</body>
+</html>

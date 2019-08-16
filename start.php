de07546fd30ea2736f0f547fed66bedf0afb3f6b
Scotchrock <54104221+Scotchrock@users.noreply.github.com>
2019-08-16 11:49:28 +0300

    456

--- start.php
+++ start.php
@@ -17,9 +17,32 @@
 					$str_1="первая строка";
 					$str_2="вторая строка"; // 123
 					echo $str_1. "".$str_2." \" \" \" ";
+					$x=12;
+					$y=34;
+					$z=4.25;
+
+					$bool_1 = $x != $z;
+
+					$bool_2 = $x != $y;
+
+					echo $bool_1. "<br />".$bool_2;
+
+					$bool_3 =  !($x == $y);
+
+					$bool_4 = $x = $y || $z < $y;
+
+					$bool_5 = $z != $y && $x < $y;
+
+					$bool_6 = $z != $x ^ $y < $z;
+
+					echo "<br/>" .$bool_3. "<br/>" .$bool_4. "<br/>" .$bool_5."<br/>".$bool_6;
+
+					$bool = !($x = ! $y && $z < $x) || $x == $y;
+
+
+					echo "<br/>".$bool;
+
 					
-
-
 
 					?>
 </body>
